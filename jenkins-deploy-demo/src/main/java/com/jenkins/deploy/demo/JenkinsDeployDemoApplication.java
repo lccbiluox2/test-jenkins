2696ca27f1e42ddcd86fece7435be9070ae475e2
541711153@qq.com <541711153lcc>
2020-03-30 22:35:43 +0800

    初始化

---
+++ jenkins-deploy-demo/src/main/java/com/jenkins/deploy/demo/JenkinsDeployDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.deploy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDeployDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsDeployDemoApplication.class, args);
+    }
+
+}
---
+++ jenkins-deploy-demo/src/test/java/com/jenkins/deploy/demo/JenkinsDeployDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.deploy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDeployDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
